04f1cc9d4bd3be2ed07433117679500e110c1c90
Daniel Lorenzo <97470453+DanielLorenzoS@users.noreply.github.com>
2022-04-17 23:48:25 -0500

    Add files via upload

---
+++ components/CrudApp.js
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+import CrudForm from "./CrudForm";
+import CrudTable from "./CrudTable";
+
+const initialDB = [
+  {
+    id: 1,
+    name: "Seiya",
+    constellation: "Pegaso",
+  },
+  {
+    id: 2,
+    name: "Shiryu",
+    constellation: "Dragón",
+  },
+  {
+    id: 3,
+    name: "Hyoga",
+    constellation: "Cisne",
+  },
+  {
+    id: 4,
+    name: "Shun",
+    constellation: "Andrómeda",
+  },
+  {
+    id: 5,
+    name: "Ikki",
+    constellation: "Fénix",
+  },
+];
+
+const CrudApp = () => {
+  const [db, setDb] = useState(initialDB);
+  const [dataToEdit, setDataToEdit] = useState(null);
+
+  const createData = (data) => {
+    data.id = Date.now();
+    setDb([...db, data]);
+  };
+  const updateData = (data) => {
+    let newData = db.map((e) => (e.id === data.id ? data : e));
+    setDb(newData);
+  };
+  const deleteData = (id) => {
+    let isDelete = window.confirm(
+      `¿Seguro que deseas eliminar el elemento ${id}?`);
+
+  if (isDelete) {
+    let newData = db.filter(e => e.id !== id);
+    setDb(newData);
+  }else{
+    return;
+  }
+}
+  return (
+    <div>
+      <h2>CRUD App</h2>
+      <CrudForm
+        createData={createData}
+        updateData={updateData}
+        dataToEdit={dataToEdit}
+        setDataToEdit={setDataToEdit}
+      />
+      <CrudTable
+        data={db}
+        setDataToEdit={setDataToEdit}
+        deleteData={deleteData}
+      />
+    </div>
+  );
+};
+
+export default CrudApp;
